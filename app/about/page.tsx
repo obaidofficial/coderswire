034703ab25a21b392c9a42d5301719cc00d94bc8
Obaid Ullah Shah <itsobaidofficial@gmail.com>
2025-12-19 13:25:00 +0500

    Leadership & We Serve section added

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import Banner from "./components/banner/Banner";
 import ClientSlider from "./components/clientsSlider/ClientSlider";
 import Pricing from "./components/Pricing/Pricing";
+import LeadershipValues from "./components/LeadershipValues/LeadershipValues";
+import WeServe from "./components/WeServe/WeServe";
 
 const Aboutpage = () => {
   return (
     <div className="overflow-hidden">
       <Banner />
       <ClientSlider />
+      <LeadershipValues />
+      <WeServe />
       <Pricing />
     </div>
   );
